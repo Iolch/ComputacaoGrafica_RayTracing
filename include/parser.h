57e0b4ed25aecf26781aa7513163a77354ebc40a
Iolanda <iccpaiva@gmail.com>
2020-03-11 23:40:50 -0300

    ADD: codigo de geração de imagem ao main

--- include/parser.h
+++ include/parser.h
@@ -13,7 +13,6 @@ struct tag_object {
   std::string tag_identifier;
   std::unique_ptr<void *> object;
 };
-// std::map<int, std::string> split(std::string str, char comma);
 std::map<std::string, std::unique_ptr<void *>> readXML(std::string file_path);
 tag_object readTag(std::string tag);
 
--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,30 @@ int main(int argc, char** argv){
 	}
 	std::map<std::string, std::unique_ptr<void *>> objects = readXML(xmlpath);
 	Film * f = static_cast<Film *>(*objects["film"]);
+	Background * b = static_cast<Background *>(*objects["background"]);
 
 	
     std::cout << "DENTRO DA MAIN " << f << std::endl; 
 	std::cout << f->getWidth() << std::endl;
+	std::cout << b->getColor() << std::endl;
 	
-	
+	// 	std::cout << ">> Creating Film Object" << std::endl;
+	// 	std::cout << "\t >> Saving at file " << f->getFilename() << std::endl;
+
+	// 	std::ofstream img(f->getFilename());
+
+	// 	int width = f->getWidth();
+	// 	int height = f->getHeight();
+	// 	img << "P3" << std::endl << width << " " << height << std::endl << "255" << std::endl;
+		
+	// 	for(int j =  height-1; j>=0; j--){
+	// 		for(int i=0; i<width; i++){
+	// 			rgb c = b->sample(float(i)/float(width), float(j)/float(height));
+	// 			img << c.r << "  " << c.g << " " << c.b << std::endl;
+	// 		}
+	// 	}
+
+	// 	std::cout << "\t >> DONE!" << std::endl;
+
 	return 0;
 }
