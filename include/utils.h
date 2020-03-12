50066a746fb9c1ac3dd16251b1e1dcede7aebac4
Iolanda <iccpaiva@gmail.com>
2020-03-11 23:23:30 -0300

    FIX: funcao split

--- include/utils.h
+++ include/utils.h
@@ -9,15 +9,20 @@ std::map<int, std::string> split(std::string str, char comma){
   int index = 0;
   std::map<int, std::string> words;
   std::string temporary;
+  bool insideString = false;
 
   for ( std::string::iterator it=str.begin(); it<=str.end(); ++it){
     
-    if(*it == comma || it == str.end()){
+    if(*it == '"'){
+    	insideString = !insideString;
+    	continue;
+    }
+    if((*it == comma || it == str.end()) && !insideString){
       words[index] = temporary;
       temporary = "";
       index += 1;
     }else{
-      if(*it != '<' and *it != '>' and *it != '"') temporary += *it;
+      if(*it != '<' and *it != '>') temporary += *it;
       else continue;
     }
   } 
--- src/background.cpp
+++ src/background.cpp
@@ -9,14 +9,14 @@ Background::Background(std::map<std::string, std::string> attributes){
 std::string Background::getType() const {return type;}
 std::string Background::getColor() const {return color;}
 std::string Background::getMapping() const {return mapping;}
-// rgb Background::sample(float i, float j){
-// 	std::map<int, std::string> c = split(getColor(), ' ');
-// 	rgb rgb_color;
-// 	rgb_color.r = int(std::stoi(c[0]*i));
-// 	rgb_color.g = int(std::stoi(c[1]*j));
-// 	rgb_color.g = int(std::stoi(c[2]*0.2));
-// 	return rgb_color;
-// }
+rgb Background::sample(float i, float j){
+	std::map<int, std::string> c = split(getColor(), ' ');
+	rgb rgb_color;
+	rgb_color.r = int(std::stoi(c[0])*i);
+	rgb_color.g = int(std::stoi(c[1])*j);
+	rgb_color.g = int(std::stoi(c[2])*0.2);
+	return rgb_color;
+}
 
 // void createFilm(std::map<std::string, std::string> attributes){
 	
