db89daf02d48bcda7a38aa86828e08ea717f491e
Iolanda <iccpaiva@gmail.com>
2020-03-16 12:53:59 -0300

    ADD: verificação das tags necessarias para geração da imagem

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,20 @@
 #include "background.cpp"
 //PARAMSET INCLUIDA DENTRO DA PARSER!
 
+bool keyExists(std::string key, std::map<std::string, std::map<std::string, std::string>> map, bool mute=false){ // alterar pra receber  map<string, void *>
+	if(!mute){
+		if(map.find(key) == map.end()) std::cout << "Nao foi possivel encontrar o objeto "+ key << std::endl;
+	}
+	return !(map.find(key) == map.end());
+}
+
 int main(int argc, char** argv){
 	int w = 200;
 	int h = 100;
 
 	char * xmlpath = argv[1];
 	if(!xmlpath){ 
-		std::cout << "Por favor, informe o caminho de um arquivo xml";
+		std::cout << "Por favor, informe o caminho de um arquivo xml" << std::endl;
 		return -1;
 	}
 	ParamSet paramSet;
@@ -23,8 +30,13 @@ int main(int argc, char** argv){
 	parser.readXML(xmlpath, &paramSet);	//LENDO O ARQUIVO, SALVANDO OS OBJETOS NA paramSet
 	std::map<std::string, std::map<std::string, std::string>> objects = paramSet.getObjects();	// ARMAZENANDO OS OBJETOS CRIADOS
 	
-	//	INSTANCIANDO OBJETOS	//
+	//	VERIFICAÇÃO DE OBJETOS	//
+	if(!keyExists("film", objects)) return -1;
+	if(!keyExists("background", objects)) return -1;
 
+
+	//	INSTANCIANDO OBJETOS	//
+	
 	std::cout << ">> Creating Film and Background Object" << std::endl;
 		Film f = {objects["film"]};	// INSTANCIANDO FILM
 		Background b = {objects["background"]};	//INSTANCIANDO BACKGROUND
